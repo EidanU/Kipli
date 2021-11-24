faebc64df00b5eebe06eb203fde4d47d35e67771
EidanU <71008360+EidanU@users.noreply.github.com>
2021-11-24 14:30:22 +0100

    add form

---
+++ components/molecules/HeaderForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-80 h-96 bg-green-50 m-11">
+    <h2>Obtenir un devis</h2>
+    <p>
+      Equipez votre etablissement avec une litterire saine,, onfortable et au
+      prix juste
+    </p>
+    <form>
+      <input type="text" required placeholder="Prenom" />
+      <input type="text" required placeholder="Nom" />
+      <input type="email" required placeholder="Email" />
+      <input type="number" required placeholder="Téléphone" />
+      <input type="text" required placeholder="Site Internet ou Entreprise" />
+      <textarea placeholder="Votre message"> </textarea>
+      <input type="submit" value="Envoyer ma demande" />
+      <span>Nous vous repondrons sous 48h</span>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderForm",
+};
+</script>
--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,11 +1,18 @@
-<template><Qualities /></template>
+<template>
+  <div class="h-screen w-screen bg-gray-50">
+    <HeaderForm />
+    <Qualities />
+  </div>
+</template>
 
 <script>
 import Qualities from "@/components/molecules/Qualities.vue";
+import HeaderForm from "@/components/molecules/HeaderForm.vue";
 export default {
   name: "Header",
   components: {
     Qualities,
+    HeaderForm,
   },
 };
 </script>
